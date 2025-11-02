5f845cefc7d5d6d27ba52fd998af6734bc5c060a
Lan <phamngoclan121204@gmail.com>
2025-11-02 19:24:03 +0700

    layout

---
+++ Thuc_Tap_Chuyen_Nganh/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+/* GET home page. */
+router.get('/pages', function(req, res, next) {
+    res.render('pages');
+});
+module.exports = router;
